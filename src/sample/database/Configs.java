5789c0d8e4057b33c0f7d8b4e4a1526ebc5146d1
kataraktax <marek.biedrzycki1984@gmail.com>
2019-06-06 00:38:58 +0200

    06.06.19 00:38

---
+++ src/sample/database/Configs.java
@@ -0,0 +1,9 @@
+package sample.database;
+
+public class Configs {
+    protected String dbHost = "localhost";
+    protected String dbPort = "3306";
+    protected String dbUser = "hbstudent";
+    protected String dbPass = "Marek";
+    protected String dbName = "mypharmacy";
+}
---
+++ src/sample/model/User.java
@@ -0,0 +1,61 @@
+package sample.model;
+
+public class User {
+
+    private String name;
+    private String lastName;
+    private String userName;
+    private String password;
+
+    public User(){
+    }
+
+    public User(String name, String lastName, String userName, String password) {
+        this.name = name;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
